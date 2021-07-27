b91ca4ba762bcad36988fa350c32e122ebd10704
Berkay-Sari <65287280+Berkay-Sari@users.noreply.github.com>
2021-07-27 19:25:52 +0300

    Add files via upload

--- website/models.py
+++ website/models.py
@@ -12,18 +12,29 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    role = db.Column(db.Integer, default = 0, onupdate = 1)
+    role = db.Column(db.Integer, default = 0)
+    #image_file = db.Column(db.String(20), default = 'default.jpg')
     subscriptions = db.relationship('Kurs', secondary = 'subs', backref = db.backref('subscribers', lazy = 'dynamic'))
+    def __repr__(self):
+        return 'User(name=%s, role=%d)' % self.first_name, self.role
+
+class Egitmen(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key = True, nullable = False)
+
 
 class Kurs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     isim = db.Column(db.String(200))
     kategori = db.Column(db.String(200))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    aciklama = db.Column(db.String(1000), nullable = True)
+    aciklama = db.Column(db.String(100), nullable = True)
+    # kisa_aciklama = db.Column(db.String(100), nullable = True)
+    # uzun_aciklama = db.Column(db.String(2000), nullable = True)
     dersler = db.relationship('Ders')
     egitmen_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     #subscribers Kurs.subscribers
+    def __repr__(self):
+        return 'Kurs(name=%s, egitmen=%d)' % (self.isim,self.egitmen_id)
 
 class Ders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
