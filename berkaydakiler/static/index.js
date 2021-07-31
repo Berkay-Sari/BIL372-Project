91da40db482747de27f827c3f23ec121718d8ade
Berkay-Sari <65287280+Berkay-Sari@users.noreply.github.com>
2021-07-31 16:52:05 +0300

    Add files via upload

---
+++ berkaydakiler/static/index.js
@@ -0,0 +1,8 @@
+function deleteKurs(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
